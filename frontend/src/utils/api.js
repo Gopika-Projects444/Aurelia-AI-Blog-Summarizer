2f221695a5d5b9ab28e084fb6f8f2c76a0258b8b
Gopika <gopikasethuram14@gmail.com>
2026-06-13 11:10:28 +0530

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: '/api',
+const API = axios.create({
+  baseURL: "https://aurelia-ai-blog-summarizer.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
